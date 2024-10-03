5055073bddad2f7fac1e8f5ed5a181f53164c61c
gatot nugroho <gearroboone@gmail.com>
2024-10-03 11:53:40 +0700

    add

--- add.php
+++ add.php
@@ -1,29 +1,38 @@
 <?php
-   	
+
+	include 'connect.php';
+   	date_default_timezone_set('Asia/Jakarta');
 	$wktu = date("Y-m-d H:i:s");
+
 	$api_uid = $_GET['uid'];
-	$data = $_GET['data'];
+	$value = $_GET['data'];
 
 	echo $api_uid;
 
-	include 'connect.php';
-	$query_sens = "SELECT * FROM device_sens WHERE uid=$api_uid";
+	$datas = "SELECT * FROM device_sens";
+	foreach ($datas as $data) {
+		$uid_sens = $data['uid'];
 
-	if ($result_sens = $connection->query($query_sens)) {
-		while ($row_sens = $result_sens->fetch_assoc()) {
-
-			$device_name = $row_sens['nama_device'];
-			$id_device = $row_sens['id'];
-
-			echo $id_device;
-		}
-	/*freeresultset*/
-	$result_sens->free();
+		echo $uid_sens;
 	}
+
+	// $query_sens = "SELECT * FROM device_sens WHERE uid=$api_uid";
+
+	// if ($result_sens = $connection->query($query_sens)) {
+	// 	while ($row_sens = $result_sens->fetch_assoc()) {
+
+	// 		$device_name = $row_sens['nama_device'];
+	// 		$id_device = $row_sens['id'];
+
+	// 		echo $id_device;
+	// 	}
+	// /*freeresultset*/
+	// $result_sens->free();
+	// }
 
 
     // $result = "INSERT INTO sens (device_send_id, data, created_at)
-    // VALUES ('$id_device', '$data', '$wktu')";
+    // VALUES ('$id_device', '$value', '$wktu')";
 
     // if(mysqli_query($conn,$result)){
     //     echo '
