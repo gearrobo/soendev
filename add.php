9e9a8243270cf6d808f2b33a373d9b5a88af72e9
gatot nugroho <gearroboone@gmail.com>
2024-10-03 10:41:57 +0700

    add

--- add.php
+++ add.php
@@ -1,8 +1,22 @@
 <?php
    	include("connect.php");
-
+	$api_uid = $_GET["uid"];
 	$data=$_GET["data"];
+	
+	echo $api_uid;
 	echo $data;
+
+	$query_sens = "SELECT * FROM device_sens ";
+
+	if ($result_sens = $connection->query($query_sens)) {
+		while ($row_sens = $result_sens->fetch_assoc()) {
+			$uid = $row_sens['uid'];
+		}
+	/*freeresultset*/
+	$result_sens->free();
+	}
+
+
 
 	// $query = "INSERT INTO 'sens' ('data', 'updated_at') 
 	// 	VALUES ('".$data."','".$waktu."')"; 
