bbabc8fb6bac9652ea121251a8e821990ef3de05
gatot nugroho <gearroboone@gmail.com>
2024-10-03 10:48:34 +0700

    add

--- add.php
+++ add.php
@@ -3,15 +3,12 @@
 	$api_uid = $_GET["uid"];
 	$data=$_GET["data"];
 
-	echo $api_uid;
-	echo "<br>";
-	echo $data;
-
 	$query_sens = "SELECT * FROM device_sens WHERE uid = $api_uid";
 
 	if ($result_sens = $connection->query($query_sens)) {
 		while ($row_sens = $result_sens->fetch_assoc()) {
-			$uid = $row_sens['uid'];
+			$device_name = $row_sens['nama_device'];
+			echo $device_name;
 		}
 	/*freeresultset*/
 	$result_sens->free();
