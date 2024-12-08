34b0e9e2c2b496bdd35e32aac3d2b0e022598236
gatot nugroho <gearroboone@gmail.com>
2024-12-08 09:57:44 +0700

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
 										<span class="icon-bar"></span>
 									</button>
                 <!-- Brand -->
-                <a class="navbar-brand page-scroll sticky-logo" href="index.html">
+                <a class="navbar-brand page-scroll sticky-logo" href="/">
                   <h1>So<span>e</span>ndev</h1>
                   <!-- Uncomment below if you prefer to use an image logo -->
                   <!-- <img src="img/logo.png" alt="" title=""> -->
