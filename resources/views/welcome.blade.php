cff5ee4997073279df19287477fa783cbdccc3d1
gatot nugroho <gearroboone@gmail.com>
2024-12-28 22:59:31 +0700

    imgslider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,7 +119,7 @@
   <div id="home" class="slider-area">
     <div class="bend niceties preview-2">
       <div id="ensign-nivoslider" class="slides">
-        <img src="{{ asset('assets/img/slider/slider1.jpg') }}" alt="" title="#slider-direction-1" />
+        <img src="{{ asset('assets/img/slider/slider11.png') }}" alt="" title="#slider-direction-1" />
         <img src="{{ asset('assets/img/slider/slider2.jpg') }}" alt="" title="#slider-direction-2" />
         <img src="{{ asset('assets/img/slider/slider3.jpg') }}" alt="" title="#slider-direction-3" />
       </div>
