84343ea7d75c4fc62e9d3fc396bcc1bdb4c2162d
gatot nugroho <gearroboone@gmail.com>
2025-04-17 10:28:35 +0700

    about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
       <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
           <div class="section-headline text-center">
-            <h2>About eBusiness</h2>
+            <h2>About </h2><h2>So<span>e</span>ndev</h2>
           </div>
         </div>
       </div>
