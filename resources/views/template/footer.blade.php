202033e9152e21ca7bb34d361c111e8b10dca98a
gatot nugroho <gearroboone@gmail.com>
2025-04-21 13:31:55 +0700

    ok

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -70,7 +70,7 @@
           <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="copyright text-center">
               <p>
-                &copy; Copyright <strong>Soendev</strong>. All Rights Reserved
+                
               </p>
             </div>
             <div class="credits">
