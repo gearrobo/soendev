03a553edc05c2aa22f2778cc83721c0940981473
gatot nugroho <gearroboone@gmail.com>
2025-10-30 10:59:36 +0700

    data

--- app/Models/SensorData.php
+++ app/Models/SensorData.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SensorData extends Model
 {
-    //
+    protected $table = 'device_sensors';
+
+    protected $fillable = ['name', 'api_key', 'status', 'temperature', 'humidity', 'latitude', 'longitude'];
 }
